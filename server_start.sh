09589de2516fb09c33bde614131d144857267877
miwa <dskipineapple@gmail.com>
2024-10-28 17:08:14 +0900

    中級編DAY10 ブロック解説と実践

--- server_start.sh
+++ server_start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cd "$(dirname "$0")"
-java -Xms4G -Xmx4G -XX:+UseG1GC -jar paper_server.jar nogui
+cd "$( dirname "$0" )"
+java -Xms4G -Xmx4G -XX:+UseG1GC -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 paper_server.jar nogui
